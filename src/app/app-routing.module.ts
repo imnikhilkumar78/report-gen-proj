b49da63714aa5394e82518ee6f341d66189e9658
Nikhil Personal Laptop <imnikhilkumar78@gmail.com>
2022-12-28 18:50:08 +0530

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'/home-dashboard', pathMatch:'full'},
+  {path:'home-dashboard', component: HomeDashboardComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
