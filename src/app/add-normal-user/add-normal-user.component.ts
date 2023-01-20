eaa1eb5dfd717bb01f548f8e338a57a1e1e57a33
Nikhil-Nandan <imnikhilkumar78@gmail.com>
2023-01-20 11:54:38 +0530

    Duplicate registration with same email disallowed

--- src/app/add-normal-user/add-normal-user.component.ts
+++ src/app/add-normal-user/add-normal-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminAuthService } from '../shared/services/admin-auth.service';
 import { NormalUsers } from '../shared/services/normal-users';
+import { UserAuthService } from '../shared/services/user-auth.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import * as crypto from 'crypto-js';
 @Component({
@@ -11,19 +12,32 @@ import * as crypto from 'crypto-js';
 export class AddNormalUserComponent implements OnInit {
   form: FormGroup;
   showMsg = false;
+  foundUser: any;
 
   constructor(
     private readonly fb: FormBuilder,
-    public authService: AdminAuthService
+    public authService: AdminAuthService,
+    private userService: UserAuthService
   ) {}
   ngOnInit(): void {}
   onSubmit() {
     const userData = this.authService.form.value;
-    const encryptedPassword = crypto.SHA512(userData.userPassword).toString();
-    userData.userPassword=encryptedPassword;
-    this.authService.createNewUser(userData);
-    this.showMsg = true;
-
-    this.authService.form.reset();
+    this.foundUser = this.userService.getUserWithEmail(
+      userData.UserEmail.toString()
+    );
+    this.foundUser.subscribe((users) => {
+      if (users.length == 0) {
+        const encryptedPassword = crypto
+          .SHA512(userData.userPassword)
+          .toString();
+        userData.userPassword = encryptedPassword;
+        this.authService.createNewUser(userData);
+        this.showMsg = true;
+        this.authService.form.reset();
+      } else {
+        alert('User with this Email already Exists!!');
+        this.authService.form.reset();
+      }
+    });
   }
 }
