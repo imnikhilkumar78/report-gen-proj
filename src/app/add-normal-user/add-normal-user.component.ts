890f5e39190ef56a2ea17ce0a15b8b5bec0a0a74
Nikhil-Nandan <imnikhilkumar78@gmail.com>
2023-01-20 12:53:28 +0530

    Fixed issues with adding user with same email

--- src/app/add-normal-user/add-normal-user.component.ts
+++ src/app/add-normal-user/add-normal-user.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AdminAuthService } from '../shared/services/admin-auth.service';
 import { NormalUsers } from '../shared/services/normal-users';
 import { UserAuthService } from '../shared/services/user-auth.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import * as crypto from 'crypto-js';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-add-normal-user',
   templateUrl: './add-normal-user.component.html',
@@ -13,6 +14,8 @@ export class AddNormalUserComponent implements OnInit {
   form: FormGroup;
   showMsg = false;
   foundUser: any;
+  numberOfUserFound: any;
+  obs: Subscription;
 
   constructor(
     private readonly fb: FormBuilder,
@@ -20,23 +23,26 @@ export class AddNormalUserComponent implements OnInit {
     private userService: UserAuthService
   ) {}
   ngOnInit(): void {}
-  onSubmit() {
+  async onSubmit() {
     const userData = this.authService.form.value;
     this.foundUser = this.userService.getUserWithEmail(
       userData.UserEmail.toString()
     );
-    this.foundUser.subscribe((users) => {
-      if (users.length == 0) {
+    this.obs = await this.foundUser.subscribe((users) => {
+      this.numberOfUserFound = users.length;
+      if (this.numberOfUserFound >= 1) {
+        alert('Already User Exists with this email');
+        this.authService.form.reset();
+      } else {
         const encryptedPassword = crypto
           .SHA512(userData.userPassword)
           .toString();
         userData.userPassword = encryptedPassword;
         this.authService.createNewUser(userData);
         this.showMsg = true;
+        alert('User Added Successfully');
         this.authService.form.reset();
-      } else {
-        alert('User with this Email already Exists!!');
-        this.authService.form.reset();
+        this.obs.unsubscribe();
       }
     });
   }
